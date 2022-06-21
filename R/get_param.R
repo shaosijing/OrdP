bab8ed6c95c6fa613e37bb09329770ec98f9516d
SJ <sshao2@nd.edu>
2022-06-21 09:39:38 -0400

    Update get_param.R
    added skewness and kuertosis (to crosslag only)

--- R/get_param.R
+++ R/get_param.R
@@ -6,6 +6,7 @@
 #' @param thresh thresholds for ordinal outcome
 #' @param autoreg_coeff autoregressive coefficient
 #' @param crosslag_coeff cross-lag coefficient
+#' @param crosslag_sk cross-lag skewness and kuertosis
 #' @param gamma_00 fixed intercept
 #' @param gamma_00_sd random intercept
 #' @param gamma_01_sd random autoregressive cofficient sd
@@ -14,7 +15,7 @@
 #' @return the estimated cross-lag coefficient and its corresponding p-value
 #' @export
 
-get_param<-function(n,numSample,numAssess,thresh,autoreg_coeff,crosslag_coeff,gamma_00,gamma_00_sd, gamma_01_sd,gamma_02_sd,Compliance){
+get_param<-function(n,numSample,numAssess,thresh,autoreg_coeff,crosslag_coeff,crosslag_sk,gamma_00,gamma_00_sd, gamma_01_sd,gamma_02_sd,Compliance){
 
   N = 1:numSample
   assess = 1:numAssess
@@ -25,6 +26,16 @@ get_param<-function(n,numSample,numAssess,thresh,autoreg_coeff,crosslag_coeff,ga
   datt$si_star = NA
   datt$pred = rnorm(nrow(datt))
 
+  if (crosslag_sk == "l"){
+    alpha<-c(4,4,4)
+    beta<-c(4,4,4)
+  } else if (crosslag_sk == "m"){
+    alpha<-c(4,4,4)
+    beta<-c(4,4,24)
+  } else if (crosslag_sk == "h"){
+    alpha<-c(4,4,4)
+    beta<-c(4,4,144)
+  }
   # autoreg <- rep(autoreg_coeff,max(N)) #leave out random effect for now
   #int<-  rnorm(numSample,gamma_00,gamma_00_sd)
   #autoreg <- rnorm(max(N),autoreg_coeff, 0.569)
@@ -47,13 +58,48 @@ get_param<-function(n,numSample,numAssess,thresh,autoreg_coeff,crosslag_coeff,ga
     crosslag <- rep(crosslag_coeff,max(N))
   } else if (gamma_02_sd != 0){
     gam <- c(gamma_00, autoreg_coeff, crosslag_coeff)
-    a<-matrix(c(gamma_00_sd,-0.54,-0.5,-0.54, gamma_01_sd, -0.5, -0.5,-0.5, gamma_02_sd),nrow = 3)
+    a<-matrix(c(1,-0.1,-0.1,-0.1, 1, -0.1, -0.1,-0.1, 1),nrow = 3)
     stdevs <- c(gamma_00_sd,gamma_01_sd,gamma_02_sd)
     b <- stdevs %*% t(stdevs)
     G <- b * a
 
-    uj <- mnormt::rmnorm(max(N), mean = gam, varcov = G)
+   # uj <- mnormt::rmnorm(max(N), mean = gam, varcov = G)
   #  betaj <- matrix(gam, nrow = max(N), ncol = 3, byrow = TRUE) + uj
+
+    Z <- mnormt::rmnorm(max(N), mean = gam, varcov = G)
+
+    U = pnorm(Z)
+    X = matrix(NA,nrow=max(N),ncol=3)
+    #for each latent var, generate a beta marginal using U and given alpha, beta params
+    for(k in 1:m){
+      X[,k] = (qbeta(U[,k],2,5)-2/7)/(sqrt(5/196)) #from chen 2019
+      #X[,i] = (qgamma(U[,i],shape=1,scale=.25)-1*.25)/(1*.25^2) #gamma dist but mean 0 var = 1
+    }
+    copBeta = function(U,alpha,beta){
+      alpha1 = alpha[1];alpha2 = alpha[2];alpha3 = alpha[3]
+      beta1 = beta[1]; beta2 = beta[2]; beta3 = beta[3]
+      X=matrix(NA,nrow=nrow(U),ncol=ncol(U))
+      mn = NULL
+      scl = NULL
+      mn[1] = alpha1/(alpha1+beta1) #mean of beta distribution
+      mn[2] = alpha2/(alpha2+beta2)
+      mn[3] = alpha3/(alpha3+beta3)
+
+
+      scl[1]=sqrt((alpha1*beta1)/((alpha1+beta1)^2*(alpha1+beta1+1))) #sd of beta distribution
+      scl[2]=sqrt((alpha2*beta2)/((alpha2+beta2)^2*(alpha2+beta2+1)))
+      scl[3]=sqrt((alpha3*beta3)/((alpha3+beta3)^2*(alpha3+beta3+1)))
+
+      for(j in 1:ncol(U)){
+        X[,j] = (qbeta(U[,j],alpha[j],beta[j])-mn[j])/scl[j] #could change qbeta if wanted diff marginal
+      }
+
+      return(X)
+    }
+
+    uj<-copBeta(U, alpha, beta) #add skewness and kuertossis only to
+                                #autoregressive and cross-lag parameters
+
     int<-uj[,1]
     autoreg<- uj[,2]
     crosslag<-uj[,3]
