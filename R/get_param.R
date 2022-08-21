ab7e17e130fae40c01fde2daf5689b83528fbf7f
SJ <sshao2@nd.edu>
2022-08-21 08:49:47 -0400

    dependency

--- R/get_param.R
+++ R/get_param.R
@@ -13,7 +13,7 @@
 #' @param gamma_02_sd random cross-lag coefficient sd
 #' @param Compliance compliance rate in percentage
 #' @return the estimated cross-lag coefficient and its corresponding p-value
-#' @importFrom covsim rIG
+#' @import covsim
 #' @export
 
 get_param<-function(n,numSample,numAssess,thresh,autoreg_coeff,crosslag_coeff,crosslag_sk,gamma_00,gamma_00_sd, gamma_01_sd,gamma_02_sd,Compliance){
@@ -68,7 +68,7 @@ get_param<-function(n,numSample,numAssess,thresh,autoreg_coeff,crosslag_coeff,cr
   #  betaj <- matrix(gam, nrow = max(N), ncol = 3, byrow = TRUE) + uj
 
 
-    uj <- rIG(max(N), sigma = G, skewness = c(0,0, crosslag_skew),
+    uj <- covsim::rIG(max(N), sigma = G, skewness = c(0,0, crosslag_skew),
         excesskurt = c(3,3, crosslag_kurt), reps = 1)[[1]]
 
     int<-uj[,1]
